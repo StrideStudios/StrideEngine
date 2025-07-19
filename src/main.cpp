341712e3476a57ef513dcc5dae781955847eb724
StrideStudios <StrideStudios868@gmail.com>
2025-07-19 01:09:43 -0400

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <cassert>
+
+#include <array>
+#include <cmath>
+#include <cstdint>
+#include <vector>
+
+#include <VkBootstrap.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vulkan/vulkan.h>
+
+#define VK_CHECK(call)                 \
+    do {                               \
+        VkResult result_ = call;       \
+        assert(result_ == VK_SUCCESS); \
+    } while (0)
+
+int main() {
+
+}
